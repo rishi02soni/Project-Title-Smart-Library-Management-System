f7900bcf33f52e145b5077c6751fcc2483e116bc
Rishi Soni <167216843+rishi02soni@users.noreply.github.com>
2026-04-30 22:11:34 +0530

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,13 @@
+public class Student extends User {
+    private int studentId;
+
+    public Student(String name, int studentId) {
+        super(name);
+        this.studentId = studentId;
+    }
+
+    @Override
+    public void displayUser() {
+        System.out.println("Student: " + name + " | ID: " + studentId);
+    }
+}
